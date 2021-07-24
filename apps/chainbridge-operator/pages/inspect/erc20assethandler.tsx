6432f18806761bcaf76e21cf9aaea1be6134f8f9
amphineko <sukareki@gmail.com>
2021-07-25 02:00:04 +0800

    [chainbridge-operator] chore(pages): optimize code
    Signed-off-by: amphineko <sukareki@gmail.com>

--- apps/chainbridge-operator/pages/inspect/erc20assethandler.tsx
+++ apps/chainbridge-operator/pages/inspect/erc20assethandler.tsx
@@ -1,25 +1,42 @@
+import { Box, Stack } from '@chakra-ui/react'
 import { useErc20AssetHandler } from '@phala/chainbridge-ethereum-react'
-import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { useMemo } from 'react'
+import { useQuery } from 'react-query'
+import { v4 as uuidv4 } from 'uuid'
 
 const Erc20AssetHandlerPage = (): JSX.Element => {
-    
+    const { query } = useRouter()
+    const address = (query['address'] instanceof Array) ? query['address'][0] : query['address']
 
-    const { contract, error } = useErc20AssetHandler('0xDf2E83f33dB8A9CcF3a00FCe18C3F509b974353D')
+    const { contract, error } = useErc20AssetHandler(address)
 
-    const [resolvedAddress, setResolvedAddress] = useState<string>()
-    useEffect(() => {
-        contract?.resolvedAddress.then((address) => setResolvedAddress(address))
-    }, [contract?.resolvedAddress])
+    const resolvedAddressQueryKey = useMemo(() => uuidv4(), [])
+    const { data: resolvedAddress } = useQuery([resolvedAddressQueryKey, address], async () => {
+        return await contract?.resolvedAddress
+    })
 
     return (
-        <>
-            <section>
-                <h2>Error</h2>
-                <p>{error?.message ?? 'No error'}</p>
-                <h2>Contract</h2>
-                <p>Address: {resolvedAddress}</p>
-            </section>
-        </>
+        <Stack>
+            <h1>Erc20AssetHandler Inspection</h1>
+
+            {error &&
+                <Box>
+                    <h2>Contract Load Error</h2>
+                    <p>{error?.message ?? 'No error'}</p>
+                </Box>
+            }
+
+            <Box as="section">
+                <p>Resolved Address: {resolvedAddress}</p>
+            </Box>
+
+            <Box as="section">
+                {
+                    // TODO: inspect deposit records
+                }
+            </Box>
+        </Stack>
     )
 }
 
